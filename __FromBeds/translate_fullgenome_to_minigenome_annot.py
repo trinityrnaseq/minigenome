b8d375ea79b93aa82df3ec767294ce97a09735b7
Brian Haas <brianjohnhaas@gmail.com>
2022-04-21 15:17:43 -0400

    update

--- __FromBeds/translate_fullgenome_to_minigenome_annot.py
+++ __FromBeds/translate_fullgenome_to_minigenome_annot.py
@@ -15,16 +15,54 @@ logger = logging.getLogger(__name__)
 
 def main():
 
-    #parser = argparse.ArgumentParser(description="translate full genome bed to the minigenome bed",
-     #                                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(description="translate full genome gtf|gff3 to the minigenome gtf|gff3",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 
-    #parser.add_argument("--fullgenome_annot", type=str, required=True, help="input fullgenome bed or gtf file to translate coords")
-    #parser.add_argument("--translation_intervals_bed", type=str, required=True, help="intervals bed file")
+    parser.add_argument("--fullgenome_annot", type=str, required=True, help="input fullgenome gtf or gff3 file to translate coords")
+    parser.add_argument("--translation_intervals", type=str, required=True, help="translation intervals tsv file")
 
-    #args = parser.parse_args()
+    args = parser.parse_args()
+
+    fullgenome_annot_filename = args.fullgenome_annot
+    translation_intervals_filename = args.translation_intervals
+
+
+    df = pd.read_csv(fullgenome_annot_filename, sep="\t", names=["Chromosome", "Source", "Type", "Start", "End", "Something", "Strand", "Dot", "Info"])
+    pd_translation = pd.read_csv(translation_intervals_filename, sep="\t")
+
+    pr_df = pr.PyRanges(df)
+    pr_translation = pr.PyRanges(pd_translation)
+    
+    pr_join = pr_df.join(pr_translation)
+
+    #pr_join.df.to_csv(fullgenome_annot_filename + ".w_translation_table.tsv", sep="\t", index=False, quoting=csv.QUOTE_NONE)
+    
+    data = pr_join.df.copy()
+    data['Start2'] = data['Start'] - data['Start_b'] + data['NewStart']
+    data['End2'] = data['End'] - data['Start_b'] + data['NewStart']
+
+    data['OrigStart'] = data['Start']
+    data['OrigEnd'] = data['End']
+
+    def add_orig_coords(row):
+        return row['Info'] + f" OrigCoords \"{row['OrigStart']}-{row['OrigEnd']}\" "
+    
+    data['Info'] = data.apply(add_orig_coords, axis=1)
+    
+        
+    
+    data['Start'] = data['Start2']
+    data['End'] = data['End2']
+    data = data[ ["Chromosome", "Source", "Type", "Start", "End", "Something", "Strand", "Dot", "Info"] ]
+
+    #data = data[ ["Chromosome", "Source", "Type", "Start", "End", "Something", "Start_b", "End_b", "NewStart", "NewEnd", "OrigStart", "OrigEnd", "Strand", "Dot", "Info"] ]
+                 
+    data.to_csv("minigenome.gtf", sep="\t", index=False, header=False, quoting=csv.QUOTE_NONE, escapechar='\\')
 
     
+
+    """
     df = pd.read_csv("AmexT_v47-AmexG_v6.0-DD.no_seqs.chr10p.gtf", sep="\t", names=["Chromosome", "Source", "Type", "Start", "End", "Something", "Strand", "Dot", "Info"])
     pd_translation = pd.read_csv("minigenome.coord_translation.tsv", sep="\t")
 
@@ -50,7 +88,7 @@ def main():
                  
     data.to_csv("AmexT_v47-AmexG_v6.0-DD.no_seqs.minigenome.gtf", sep="\t", index=False, header=False, quoting=csv.QUOTE_NONE)
 
-    
+    """
 
 
     """

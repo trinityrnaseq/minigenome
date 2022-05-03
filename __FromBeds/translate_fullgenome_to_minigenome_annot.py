ec54474ab19467adb319e4b1c7f1f2c1bfac2031
Brian Haas <brianjohnhaas@gmail.com>
2022-05-03 15:16:15 -0400

    run on each contig via groupby

--- __FromBeds/translate_fullgenome_to_minigenome_annot.py
+++ __FromBeds/translate_fullgenome_to_minigenome_annot.py
@@ -35,24 +35,49 @@ def main():
 
     df = pd.read_csv(fullgenome_annot_filename, sep="\t", names=["Chromosome", "Source", "Type", "Start", "End", "Something", "Strand", "Dot", "Info"])
 
-
     logger.info("parsing {}".format(translation_intervals_filename))
-    pd_translation = pd.read_csv(translation_intervals_filename, sep="\t")
+    pd_translation = pd.read_csv(translation_intervals_filename, sep="\t") # already has the Chromosome, Start, End headings
 
 
-    logger.info("joining based on feature coordinate overlaps")
-    pr_df = pr.PyRanges(df)
-    pr_translation = pr.PyRanges(pd_translation)    
-    pr_join = pr_df.join(pr_translation, nb_cpu=CPU)
+    # restrict to those annotation features that are on coordinate-transfer contigs.
+    chromosomes_can_be_translated = pd_translation['Chromosome'].unique().tolist()
+
+    pd_translation.set_index('Chromosome', drop=False, inplace=True)
+
+    df = df[ df['Chromosome'].isin(chromosomes_can_be_translated) ]
     
+
+    def overlap_by_contig(group_df):
+        #print(group_df)
+        #print(group_df.shape)
+        chromosome = group_df['Chromosome'].unique().tolist()[0]
+
+        print(chromosome)
+        
+        translation_df = pd_translation.loc[[chromosome]]
+        #print(translation_df)
+        
+        pr_group_df = pr.PyRanges(group_df)
+        pr_translation = pr.PyRanges(translation_df)
+
+        pr_join = pr_group_df.join(pr_translation)
+
+        return pr_join.df.copy()
+    
+    
+    logger.info("joining based on feature coordinate overlaps by chromosome")
+    data = df.groupby('Chromosome', as_index=False).apply(overlap_by_contig)
+
     logger.info("assigning adjusted coordinates")
-    data = pr_join.df.copy()
     data['Start2'] = data['Start'] - data['Start_b'] + data['NewStart']
     data['End2'] = data['End'] - data['Start_b'] + data['NewStart']
 
-    data['OrigStart'] = data['Start']
-    data['OrigEnd'] = data['End']
 
+    logger.info("adding original coordinates as an annotation")
+    
+    data['OrigStart'] = data['Start'].astype(int)
+    data['OrigEnd'] = data['End'].astype(int)
+    
     def add_orig_coords(row):
         return row['Info'] + f" OrigCoords \"{row['OrigStart']}-{row['OrigEnd']}\" "
     
@@ -60,13 +85,13 @@ def main():
     
         
     
-    data['Start'] = data['Start2']
-    data['End'] = data['End2']
+    data['Start'] = data['Start2'],astype(int)
+    data['End'] = data['End2'].astype(int)
     data = data[ ["Chromosome", "Source", "Type", "Start", "End", "Something", "Strand", "Dot", "Info"] ]
 
     logger.info("writing output gtf: {}".format(output_gtf))
-    data.to_csv("minigenome.gtf", sep="\t", index=False, header=False, quoting=csv.QUOTE_NONE, escapechar='\\')
-
+    data.to_csv(output_gtf, sep="\t", index=False, header=False, quoting=csv.QUOTE_NONE, escapechar='\\')
+    
 
     sys.exit(0)
 
